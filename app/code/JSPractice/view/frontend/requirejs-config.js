10adf72e14bc3b2d25f09acf0ea466c31e25f89c
memecheme <89563189+memecheme@users.noreply.github.com>
2022-05-18 17:40:33 +0300

    Delete app/code/JSPractice directory

--- app/code/JSPractice/view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    config: {
-        mixins: {
-            'Magento_Catalog/js/catalog-add-to-cart': {
-                'Denis_JSPractice/js/catalog-add-to-cart-mixin': true
-            },
-        }
-    }
-};
